4d87631ff4572609c8c1078a6736ecab81091521
Trevoltre <121647729+Trevoltre@users.noreply.github.com>
2025-11-09 16:49:55 +0300

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,32 @@
+import type { NextConfig } from "next";
+import path from "node:path";
+
+const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
+
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'slelguoygbfzlpylpxfs.supabase.co',
+      },
+    ],
+  },
+  
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
+  },
+  turbopack: {
+    rules: {
+      "*.{jsx,tsx}": {
+        loaders: [LOADER]
+      }
+    }
+  }
+};
+
+export default nextConfig;
+// Orchids restart: 1762572825814
